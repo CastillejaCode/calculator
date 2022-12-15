1009032f2c2375b27cf93e20743d04391079e751
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-15 00:41:15 -0800

    Add buttons changing display

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 "use strict";
 
+const display = document.querySelector(".display");
+const clear = document.querySelector(".clear");
+const buttons = document.querySelectorAll(".button");
+
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 const operate = (x, y, fx) => fx(x, y);
+
+buttons.forEach((e) =>
+	e.addEventListener("click", function () {
+		display.textContent += this.textContent;
+		display.value = display.textContent;
+		console.log(display.value);
+	})
+);
+
+clear.addEventListener("click", function () {
+	display.textContent = "";
+});
