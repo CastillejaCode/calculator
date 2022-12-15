7cbbf753d6b10da2cf47c1cceb3d6ea455bf3cf4
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-14 23:21:00 -0800

    Add basic calculator operators

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+const operate = (x, y, fx) => fx(x, y);
