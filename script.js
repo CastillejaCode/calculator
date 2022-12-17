0830903cee915ac1e9e71a312e5dbae3a7eed798
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-16 21:47:29 -0800

    Add basic calcultor functionality

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
 
-const display = document.querySelector(".display");
-const clear = document.querySelector(".clear");
-const buttons = document.querySelectorAll(".button");
+const displayElement = document.querySelector(".display");
+const btnClear = document.querySelector(".clear");
+const btns = document.querySelectorAll(".button");
+const btnOperator = document.querySelectorAll(".operator");
+const btnEqual = document.querySelector(".equal");
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
@@ -11,14 +13,43 @@ const divide = (x, y) => x / y;
 
 const operate = (x, y, fx) => fx(x, y);
 
-buttons.forEach((e) =>
+let firstNumber;
+let secondNumber;
+let operator;
+let display;
+
+const changeDisplay = function () {
+	displayElement.textContent += this.textContent;
+	display = displayElement.value = displayElement.textContent;
+};
+
+const clearDisplay = function () {
+	displayElement.textContent = "";
+};
+
+btns.forEach((e) => e.addEventListener("click", changeDisplay));
+// btns.forEach((e) => e.addEventListener("click", clearDisplay));
+
+btnClear.addEventListener("click", clearDisplay);
+
+//Save first number to variable when operator pressed
+btnOperator.forEach((e) =>
 	e.addEventListener("click", function () {
-		display.textContent += this.textContent;
-		display.value = display.textContent;
-		console.log(display.value);
+		firstNumber = +display.replace(e.textContent, "");
+		operator = this.textContent;
 	})
 );
+//When equal pressed, parse variables into operate fx
+btnEqual.addEventListener("click", function () {
+	secondNumber = +display.replace(firstNumber, "").replace(/[+-/*]/g, "");
 
-clear.addEventListener("click", function () {
-	display.textContent = "";
+	if (operator === "+") operator = add(firstNumber, secondNumber);
+	if (operator === "-") operator = subtract(firstNumber, secondNumber);
+	if (operator === "*") operator = multiply(firstNumber, secondNumber);
+	if (operator === "/") operator = divide(firstNumber, secondNumber).toFixed(1);
+
+	displayElement.textContent = operator;
+	operator = false;
 });
+
+//After display, reset display when press another number
