f3694e4b8ede3e103f8d07cbac4b4cd8819a04fc
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-18 17:14:06 -0800

    Clean up code

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ let subOperation;
 let answer = "";
 let count = 0;
 
+//////Functions//////
 const checkError = function () {
 	if (answer == "Infinity") {
 		answer = "DIVIDE BY 0 ERROR";
@@ -61,6 +62,9 @@ const checkDecimal = function () {
 	if (answer % 1 != 0) answer = Math.round(answer * 100) / 100;
 };
 
+/////////Event Handlers/////////
+
+//Update display with each button input
 buttons.forEach((e) =>
 	e.addEventListener("click", (e) => {
 		operation += e.target.value;
@@ -68,18 +72,12 @@ buttons.forEach((e) =>
 	})
 );
 
-document.addEventListener("keydown", (e) => {
-	console.log(e);
-	operation += e.target.value;
-	updateDisplay();
-});
-
+//Create first term with operator button
 btnOperator.forEach((e) =>
 	e.addEventListener("click", (e) => {
 		//Allow adding answer to new number
 		if (answer) {
 			operation = answer + `${e.target.value}`;
-
 			answer = "";
 			updateDisplay();
 		}
@@ -97,7 +95,6 @@ btnOperator.forEach((e) =>
 			checkError();
 			operation = answer + `${e.target.value}`;
 			if (operation.slice(-1) == "√") operation = `${e.target.value}` + answer;
-
 			answer = "";
 			updateDisplay();
 		}
@@ -109,10 +106,13 @@ btnOperator.forEach((e) =>
 		if (e.target.value === "+") operator = add;
 		if (e.target.value === "^") operator = exponent;
 		if (e.target.value === "√") operator = squareroot;
+
+		//Used to create term2
 		subOperation = operation;
 	})
 );
 
+//Create equal button functionality
 btnEqual.addEventListener("click", () => {
 	term2 = Number(operation.replace(subOperation, ""));
 
@@ -122,9 +122,9 @@ btnEqual.addEventListener("click", () => {
 		answer = "SYNTAX ERROR";
 		resetScreen();
 	} else answer = operate(term1, term2, operator);
+
 	checkDecimal();
 	updateDisplay();
-	console.log(answer);
 });
 
 btnClear.addEventListener("click", (e) => {
@@ -137,56 +137,19 @@ btnDelete.addEventListener("click", (e) => {
 	updateDisplay();
 });
 
-// const changeDisplay = function () {
-// 	displayElement.textContent += this.textContent;
-// 	display = displayElement.value = displayElement.textContent;
-// };
+//////End//////
 
-// const clearDisplay = function () {
-// 	displayElement.textContent = "";
-// 	firstNumber = secondNumber = operator = answer = "";
-// };
+//Potential keyabord support
 
-// const operateAnswer = function () {
-// 	secondNumber = Number(
-// 		display.replace(firstNumber, "", 1).replace(/[+/*-]/g, "")
-// 	);
-// 	console.log(secondNumber);
+// document.addEventListener("keydown", (e) => {
+// 	console.log(e.key);
+// 	if (!e.key.match(/[0-9]/g) && !e.key.match(/[*^+/√-]/g)) return;
+// 	else operation += e.key;
+// 	updateDisplay();
+// });
 
-// 	if (operator === "+") answer = add(firstNumber, secondNumber);
-// 	if (operator === "-") answer = subtract(firstNumber, secondNumber);
-// 	if (operator === "*") answer = multiply(firstNumber, secondNumber);
-// 	if (operator === "/") answer = divide(firstNumber, secondNumber).toFixed(1);
-
-// 	count = 0;
-
-// 	if (answer === "Infinity") {
-// 		displayElement.textContent = `You can't divide by 0!`;
-// 	} else displayElement.textContent = answer;
-// };
-
-// btns.forEach((e) => e.addEventListener("click", changeDisplay));
-// // btns.forEach((e) => e.addEventListener("click", clearDisplay));
-
-// btnClear.addEventListener("click", clearDisplay);
-
-// btnOperator.forEach((e) =>
-// 	e.addEventListener("click", function () {
-// 		count++;
-// 		if (count > 1) {
-// 			operateAnswer();
-// 			displayElement.textContent += this.textContent;
-// 			firstNumber = answer;
-// 			operator = this.textContent;
-// 			count = 0;
-// 		} else {
-// 			firstNumber = display.replace(this.textContent, "");
-// 			operator = this.textContent;
-// 		}
-// 	})
-// );
-// //When equal pressed, parse variables into operate fx
-
-// btnEqual.addEventListener("click", operateAnswer);
-
-// //After display, reset display when press another number
+// document.addEventListener("keydown", (e) => {
+// 	if (e.key === "Backspace")
+// 		operation = operation.substring(0, operation.length - 1);
+// 	updateDisplay();
+// });
